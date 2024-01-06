4c82bcaf7656617905553a94392027b68c0efd0c
Keval <typicaljoe111@users.noreply.github.com>
2024-01-06 16:09:02 +0530

    Added URL router

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
 import App from './App.jsx'
+import Login from './Login.jsx'
+import Signup from './Signup.jsx'
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />
+	},
+	{
+		path: '/login',
+		element: <Login />
+	},
+	{
+		path: '/signup',
+		element: <Signup />
+	}
+]);
 
 const rootElement = document.getElementById('root');
 ReactDOM.createRoot(rootElement).render(
 	<React.StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</React.StrictMode>,
 )
 
